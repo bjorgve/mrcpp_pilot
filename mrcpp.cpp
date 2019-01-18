8e3d42956275c6d8d3e1901e3e9e27833be66bb3
Magnar Bjørgve <magnbjor@gmail.com>
2019-01-18 14:56:13 +0100

    Derivative test

--- mrcpp.cpp
+++ mrcpp.cpp
@@ -5,16 +5,8 @@
 #include "MRCPP/MWFunctions"
 #include "MRCPP/Gaussians"
 #include "MRCPP/MWOperators"
-#include "MRCPP/core/LegendreBasis.h"
-#include "MRCPP/core/QuadratureCache.h"
-#include "MRCPP/core/InterpolatingBasis.h"
-#include "MRCPP/core/ScalingBasis.h"
-#include "../include/config.h"
 
 #include <fstream>
-
-using Eigen::MatrixXd;
-using Eigen::VectorXd;
 
 // This is your personal sandbox to test out ideas.
 // See the examples/ directory for inspiration.
@@ -30,72 +22,83 @@ const auto D = 3; // Dimensions
 int main(int argc, char **argv) {
     Timer timer;
 
-    // Plots
     // Initialize printing
     int printlevel = 0;
     Printer::init(printlevel);
     Printer::printEnvironment();
     Printer::printHeader(0, "MRCPP pilot code");
 
+    // Plots
+    int nPts = 1000;
+    double a[3] = {0.0, 0.0, 0.0};
+    double b[3] = {20.0, 0.0, 0.0};
+
+    Plotter<3> plot;
+    plot.setNPoints(nPts);
+    plot.setRange(a, b);
+
+{
+    auto basis = InterpolatingBasis(order);
+    auto sf_x = 2.0*pi;
+    auto sf = std::array<double, D>{sf_x, sf_x, sf_x};
+    auto world = BoundingBox<D>(sf, true);
+
+    // auto world = BoundingBox<D>(sf, true);
+    auto MRA = MultiResolutionAnalysis<D>(world, basis, max_depth);
 
 
-    std::string file;
-    std::string path = MW_FILTER_DIR;
-    file = path + "/L_ph_deriv_1.txt";
+    auto g_func = [] (const mrcpp::Coord<D> &r) {
+        // auto p = pi*1.0;
+        // return cos(p*r[0])*cos(p*r[1])*cos(p*r[2]);
+        return cos(r[0])*cos(r[1])*cos(r[2]);
+    };
+    auto dg_func = [] (const mrcpp::Coord<D> &r) {
+        // auto p = pi*1.0;
+        // return cos(p*r[0])*cos(p*r[1])*cos(p*r[2]);
+        return sin(r[0])*cos(r[1])*cos(r[2]);
+    };
 
-    std::ifstream ifs(file.c_str());
-    std::string line;
+    FunctionTree<D> g_tree(MRA);
+    FunctionTree<D> dg_tree(MRA);
+    FunctionTree<D> dg_exact_tree(MRA);
+    ABGVOperator<D> Der(MRA, 0.0, 0.0);
+
+    project<D>(prec, g_tree, g_func);
+    project<D>(prec, dg_exact_tree, dg_func);
+
+   apply(dg_tree, Der, g_tree, 0);
 
 
-    std::ofstream file_w("test.txt");
-    file_w.precision(16);
-    file_w.setf(std::ios::scientific);
-
-// auto give_basis(int order) {
-//     if (order == 0) return 1.0;
-//     return LegendreBasis(order);
+    // println(0, f_tree.evalf({0.0, 0.0, 0.0}));
+    println(0, g_tree.evalf({0.0, 0.0, 0.0}));
+    plot.linePlot(dg_tree, "dg_tree_sf");
+    MRA.print();
+}
+// {
+//     auto basis = InterpolatingBasis(order);
+//     auto sf_x = 1.0;
+//     auto sf = std::array<double, D>{sf_x, sf_x, sf_x};
+//     auto world = BoundingBox<D>(-4, {0, 0, 0}, {2, 2, 2});
+//
+//     // auto world = BoundingBox<D>(sf, true);
+//     auto MRA = MultiResolutionAnalysis<D>(world, basis, max_depth);
+//
+//     auto f_func = [] (const mrcpp::Coord<D> &r) {
+//         auto p = 1.0;
+//         return cos(p*r[0])*cos(p*r[1])*cos(p*r[2]);
+//     };
+//
+//     FunctionTree<D> f_tree(MRA);
+//     FunctionTree<D> g_tree(MRA);
+//     PoissonOperator P(MRA, prec);
+//     project<D>(prec, f_tree, f_func);
+//
+//     apply(prec, g_tree, P, f_tree);
+//     plot.linePlot(g_tree, "g_tree");
+//
+//     MRA.print();
 // }
-
-    for (auto kp1 = 2; kp1 < 5; kp1++) {
-
-        auto basis = LegendreBasis(kp1-1);
-
-        getQuadratureCache(qCache);
-        const VectorXd &roots = qCache.getRoots(kp1);
-        const VectorXd &weights = qCache.getWeights(kp1);
-
-        getline(ifs, line);
-        if (kp1 != std::stoi(line)) MSG_FATAL("Something wrong");
-        MatrixXd data = MatrixXd::Zero(3*kp1, kp1);
-        for (int i = 0; i < 3*kp1; i++) {
-            getline(ifs, line);
-            std::istringstream iss(line);
-            for (int j = 0; j < kp1; j++) {
-                iss >> data(i, j);
-            }
-        }
-
-        MatrixXd S_p1 = data.block(0*kp1, 0, kp1, kp1);
-        MatrixXd S_0 = data.block(1*kp1, 0, kp1, kp1);
-        MatrixXd S_m1 = data.block(2*kp1, 0, kp1, kp1);
-
-        MatrixXd S = MatrixXd::Zero(kp1, kp1);
-
-        for (auto row = 0; row < kp1; row++) {
-            for (auto column = 0; column < kp1; column++) {
-                S(row, column) = basis.getFunc(row).evalf(roots[column])*weights[column];
-            }
-        }
-
-        // Writing to new file
-        file_w << kp1 << '\n';
-        file_w << S.inverse()*S_p1*S << '\n';
-        file_w << S.inverse()*S_0*S<< '\n';
-        file_w << S.inverse()*S_m1*S << '\n';
-
-
-        // println(0, data);
-
-    }
+    timer.stop();
+    Printer::printFooter(0, timer, 2);
     return 0;
 }
